f79f3c98ae35d38f7b6214a3e31782ffd4737730
kkig <ex@sample.com>
2025-05-29 11:03:25 +0900

    refactor: seperated event permissions event/permissions.py

--- backend/events/views.py
+++ backend/events/views.py
@@ -2,22 +2,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, permissions, viewsets
 
 from .models import Event
+from .permissions import IsOrganizerOrReadOnly
 from .serializers import EventSerializer
-
-
-class IsOrganizerOrReadOnly(permissions.BasePermission):
-    """
-    Custom permission to allow only the event organizer to edit it.
-    Others can only read.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # Allow safe methods (GET, HEAD, OPTIONS)
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Allow only if the object belong to the user
-        return obj.organizer == request.user
 
 
 class EventViewSet(viewsets.ModelViewSet):
