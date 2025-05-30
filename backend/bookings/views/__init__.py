1fb999678a6a00e29b0eb3c627b218a5fa1af798
kkig <ex@sample.com>
2025-05-30 16:51:28 +0900

    feat: add booking cancellation

--- backend/bookings/views/__init__.py
+++ backend/bookings/views/__init__.py
@@ -1,5 +1,11 @@
 from .create import BookingCreateView
 from .list import BookingListView
 from .retrieve import BookingDetailView
+from .update import BookingCancelView
 
-__all__ = ["BookingCreateView", "BookingListView", "BookingDetailView"]
+__all__ = [
+    "BookingCreateView",
+    "BookingListView",
+    "BookingDetailView",
+    "BookingCancelView",
+]
