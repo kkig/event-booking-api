7bed870d4728c236e041996fc823fb084a110725
kkig <ex@sample.com>
2025-05-30 13:58:48 +0900

    feat: add view to show booking detail

---
+++ backend/bookings/views/retrieve.py
@@ -0,0 +1,16 @@
+from rest_framework import generics, permissions
+
+from backend.bookings.models import Booking
+from backend.bookings.serializers import BookingDetailSerializer
+
+
+class BookingDetailView(generics.RetrieveAPIView):
+    serializer_class = BookingDetailSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return (
+            Booking.objects.filter(user=self.request.user)
+            .select_related("event")
+            .prefetch_related("items__ticket_type")
+        )  # Optimizing booking.all() + booking.items.all() + item.ticket_type
