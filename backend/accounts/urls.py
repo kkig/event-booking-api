27ade75eee18e717d5ba4d42b3ade3a7b769516f
kkig <ex@sample.com>
2025-06-01 10:10:15 +0900

    chore: url login to login/

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -8,7 +8,7 @@ app_name = "accounts"
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     # Authenticate and return access + refresh token pair - token based auth
-    path("login", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     # Sends refresh token and get access token
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
